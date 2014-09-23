2fd094f7b2be8b1d07ab5f8dbc93fdeb64c488a4
Victor Widell <victor@topmost.se>
2014-09-23 22:01:37 +0200

    First commit.

---
+++ index.js
@@ -0,0 +1,129 @@
+"use strict";
+
+var pg = require('pg');
+var Q = require('q');
+
+// TODO: Switch to connection pooling?
+// Transactions with pooling is kind of difficult to get right. Must 
+// have an automatic api for it then, since you can't have all that
+// code on the api consumer side.
+// https://github.com/brianc/node-postgres/wiki/Transactions
+
+module.exports = {
+
+	connect: function (connectionString) {
+
+		var deferred = Q.defer();
+
+		var client = new pg.Client(connectionString);
+
+		client.connect(function (error) {
+
+			if (error) {
+
+				deferred.reject(error);
+				return;
+			}
+
+			deferred.resolve({
+
+				end: function () {
+
+					client.end();
+				},
+
+
+				query: query,
+
+
+				queryGenerated: queryGenerated,
+
+
+				insert: function (tableDefinition, objectOrArray) {
+
+					return Q().then(function () {
+					
+						return queryGenerated(
+							tableDefinition
+								.insert(objectOrArray)
+						);
+					});
+				},
+
+
+				deleteWhere: function (tableDefinition, conditions) {
+
+					return Q().then(function () {
+
+						return queryGenerated(
+							tableDefinition
+								.delete()
+								.where(conditions)
+						);
+					});
+				},
+
+
+				readOneWhere: function (tableDefinition, conditions) {
+
+					return Q().then(function () {
+
+						return queryGenerated(
+							tableDefinition
+								.select()
+								.where(conditions)
+						).then(function (rows) { return rows[0]; });
+					});
+				},
+
+
+				readAll: function (tableDefinition) {
+
+					return Q().then(function () {
+
+						return queryGenerated(
+							tableDefinition
+								.select()
+						);
+					});
+				}
+			});
+
+			function query (SQL, parameters) {
+
+				// Wrap with Q().then to make exceptions work.
+				var outerArguments = arguments;
+				return Q().then(function () {
+
+					return Q
+						.nbind(client.query, client)(SQL, parameters)
+						.catch(function (error) {
+
+							// Just a convenient place to log this.
+							error.SQL        = SQL;
+							error.parameters = parameters;
+
+							// Rethrow the error since it wasn't handled.
+							throw error;
+
+						}).then(function (result) {
+
+							return result.rows;
+						});
+				});
+			}
+
+			function queryGenerated (generated) {
+
+				return Q().then(function () {
+
+					var SQLAndValues = generated.toQuery();
+
+					return query(SQLAndValues.text, SQLAndValues.values);
+				});
+			}
+		});
+
+		return deferred.promise;
+	}
+};
---
+++ readme.md
@@ -0,0 +1,60 @@
+`connect` returns a promise of a connection. It has these methods:
+
+* `query` takes an SQL sting and optional parameters, returning a promise of the result.
+* `end` disconnects.
+* `queryGenerated` takes a `node-sql`-object, returning a promise of the result.
+* insert (tableDefinition, objectOrArray) - takes a `node-sql` table definition and an object to insert.
+
+Basic example:
+
+    progres.connect(conString).then(function (client) {
+
+        return client
+            .query(SQL)
+            .finally(client.end);
+
+    }).done();
+
+
+
+
+
+An example Express route:
+	
+    var connectionString =
+        'postgres://user:password@localhost/database';
+
+    var posts = sql.define({
+        name: 'posts',
+        columns: [
+            "id",
+            "slug",
+            "content",
+            "timePublished",
+            "keywords"
+        ]
+    })
+
+    module.exports.allPosts = function (event, res) {
+
+        progres
+            .connect(connectionString)
+            .then(function (client) {
+
+                return client
+                    .readAll(posts)
+                    .finally(client.end);
+
+            }).done(function () {
+
+                res.end();
+
+            }, function (error) {
+
+                res.send(500);
+                console.error(
+                    'Error in allPosts:',
+                    error
+                );
+            });
+    }
