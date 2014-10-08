451be65c8472df4269a0c65514d38b1357a8415b
Victor Widell <victor@topmost.se>
2014-10-08 23:39:26 +0200

    Clarified test.

--- progres/test.js
+++ progres/test.js
@@ -3,20 +3,21 @@
 var Q = require('q');
 var progres = require("./index.js");
 
-Q.all([
-	progres.connect("postgres://localhost", function (client) {
 
-		var handwrittenSQL = "SELECT 'passed' AS result";
+// Connect to postges.
+progres.connect("postgres://localhost", function (client) {
 
-		return client.query(handwrittenSQL).then(function (rows) {
+	// Run a query.
+	return client.query("SELECT 'passed' AS result").then(function (rows) {
 
-			if (rows[0].result != "passed") {
+		// Ensure we get the expected result back.
+		if (rows[0].result != "passed") {
 
-				throw new Error("Didn't pass.");
-			}
-		});
-	})
-]).done(function () {
+			throw new Error("Didn't pass.");
+		}
+	});
+})
+.done(function () {
 
 	console.log("Everything OK.");
 
