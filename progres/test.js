ccf3c16bbf6e3ed840947ee6da26fa6b44938562
Victor Widell <victor@topmost.se>
2014-10-09 18:47:18 +0200

    Added tests.

--- progres/test.js
+++ progres/test.js
@@ -4,25 +4,152 @@ var Q = require('q');
 var progres = require("./index.js");
 
 
-// Connect to postges.
-progres.connect("postgres://localhost", function (client) {
+[
 
-	// Run a query.
-	return client.query("SELECT 'passed' AS result").then(function (rows) {
+	// Errors in the job should propagate.
+	function () {
 
-		// Ensure we get the expected result back.
-		if (rows[0].result != "passed") {
+		return progres.connect("postgres://localhost", function (client) {
 
-			throw new Error("Didn't pass.");
-		}
+			// Exception in the job.
+			throw "passed";
+
+		}).then(function () {
+
+			throw new Error("Errors in the job should propagate.");
+
+		}, function (error) {
+
+			if (error != "passed") {
+
+				// Unexpected error.
+				throw error;
+			}
+		})
+	},
+
+
+	// postgresClient.end() should be called after the job is done.
+	function () {
+
+		var calledEnd = false;
+
+		return progres.connect("postgres://localhost", function (client) {
+
+			// Attach a hook directly to the postgres client.
+			var oldEnd = client.postgresClient.end;
+			client.postgresClient.end = function () {
+
+				oldEnd.call(client.postgresClient);
+
+				calledEnd = true;
+			};
+
+			return Q();
+
+		}).then(function () {
+
+			if (!calledEnd) {
+
+				throw new Error("postgresClient.end() should be called after the job is done.");
+			}
+		})
+	},
+
+
+	// Queries should return the resulting rows.
+	function () {
+
+		return progres.connect("postgres://localhost", function (client) {
+
+			// Run a query.
+			return client.query("SELECT 'passed' AS column_name;").then(function (rows) {
+
+				// Ensure we get the expected result back.
+				if (rows[0].column_name !== "passed") {
+
+					throw new Error("Queries should return the resulting rows.");
+				}
+			});
+		})
+	},
+
+
+	// Broken SQL should propagate the error.
+	function () {
+
+		return progres.connect("postgres://localhost", function (client) {
+
+			var brokenSql = "broken sql";
+
+			// Run a query.
+			return client.query(brokenSql).then(function () {
+
+				throw new Error("Broken SQL should propagate the error.");
+
+			}, function (error) {
+
+				// The broken SQL should be part of the thrown error.
+				if (error.SQL !== brokenSql) {
+
+					throw new Error("Queries should return the resulting rows.");
+				}
+			})
+		})
+	},
+
+
+	// Broken SQL should propagate the error.
+	function () {
+
+		return progres.connect("postgres://localhost", function (client) {
+
+			var brokenSql = "broken sql";
+
+			// Run a query.
+			return client.query(brokenSql).then(function () {
+
+				throw new Error("Broken SQL should propagate the error.");
+
+			}, function (error) {
+
+				// The broken SQL should be part of the thrown error.
+				if (error.SQL !== brokenSql) {
+
+					throw new Error("Queries should return the resulting rows.");
+				}
+			})
+		})
+	},
+
+
+	// An exception should be thrown if the job doesn't return a promise.
+	// (Or the client might be ended prematurely - dangerous.)
+	function () {
+
+		return progres.connect("postgres://localhost", function (client) {
+
+			return "not a promise";
+
+		}).then(function () {
+
+			throw new Error("An exception should be thrown if the job doesn't return a promise.");
+
+		}, function (error) {
+
+			// Passed the test.
+		})
+	}
+
+]
+	// Run in sequence.
+	.reduce(function (soFar, next) { return soFar.then(next); }, Q())
+	.done(function () {
+
+		console.log("Everything OK.");
+
+	}, function (error) {
+
+		console.error(error);
+		process.exit(1);
 	});
-})
-.done(function () {
-
-	console.log("Everything OK.");
-
-}, function (error) {
-
-	console.error(error);
-	process.exit(1);
-});
